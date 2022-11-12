6cef31ee5f0eb8d5c716f3b35c34952069aac6a4
zinface <zinface@163.com>
2022-11-12 23:05:54 +0800

    repo: 添加 list 子命令实现逻辑的注释

--- extenstions/youwant/list.go
+++ extenstions/youwant/list.go
@@ -14,18 +14,23 @@ var llog = &logs.Logs{
 	Prefix: "list",
 }
 
+// ListHandler 列出可用项
 func ListHandler(cmd *cobra.Command, args []string) {
+	// 获取魔法配置文件位置
 	useConfigFile := useConfigFilePathDefaultLocal(cmd)
 
+	// 如果配置文件不存在将打印配置文件不存在
 	if !utils.FileExists(useConfigFile) {
 		llog.ConfigNotExist(useConfigFile)
 		return
 	}
 
+	// 获取所有条目信息
 	wants, err := models.LoaderYouwantsFromFile(useConfigFile)
-	if err != nil {
+	if err != nil { // 出现异常将进行停止
 		panic(err)
 	}
+	// 打印 编号，指令数量，文件数量，条目名称
 	for i := 0; i < len(wants); i++ {
 		var want = wants[i]
 		fmt.Printf("%v: (%v个指令,%v个文件): %v \n", i, len(want.Template.Shell.Commands), len(want.Template.Files), want.Label)
--- extenstions/youwant/youwant.go
+++ extenstions/youwant/youwant.go
@@ -28,6 +28,8 @@ var async bool = false
 var global bool = false
 
 // configArguments 配置内部参数
+//
+// 尝试配置 -g/--global, -a/--async 等参数， 并应用到 global/async 全局变量
 func configArguments(cmd *cobra.Command, args []string) {
 	async, _ = cmd.Flags().GetBool("async")
 	global, _ = cmd.Flags().GetBool("global")
@@ -47,20 +49,31 @@ func useConfigFilePath(cmd *cobra.Command) string {
 }
 
 // useConfigFilePathDefaultLocal 获取以本地优先的配置文件
+//
+// 逻辑较为复杂，在某些情况下算是有不算bug的bug。
+// 例如在未指定 -g 时却因本地配置不存在而使用了全局配置，进行了除添加之外的修改操作而在本地创建了副本。
 func useConfigFilePathDefaultLocal(cmd *cobra.Command) string {
+	// 尝试配置 -g/--global 等参数， 并应用到 global 全局变量
 	configArguments(cmd, []string{})
+
+	// 尝试获取用户配置文件路径
 	global_youwant_json, err := ycm.GetUserConfigFilePath()
+	// 如果明确指定了 -g 参数，并且全局配置没有异常将使用'全局配置文件路径'
 	if global {
-		if err != nil {
+		// 明确指定了 -g 参数，但出现了异常
+		if err != nil { // 将尝试使用本地配置文件路径，这是无奈的逻辑
 			return youwant_json
 		}
+		// 全局配置文件路径
 		return global_youwant_json
 	} else {
+		// 如果本地配置文件不存在，将转为使用全局配置文件
 		if !utils.FileExists(youwant_json) {
 			global = true
 			return global_youwant_json
 		}
 	}
+	// 默认将使用本地文件路径
 	return youwant_json
 }
 
