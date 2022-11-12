a2fb2793b0d0c8e96b98963cf1b1ac1f9ea5ac67
zinface <zinface@163.com>
2022-11-12 23:12:10 +0800

    repo: 添加 init 子命令实现逻辑的注释

--- extenstions/youwant/init.go
+++ extenstions/youwant/init.go
@@ -15,22 +15,26 @@ var ilog = logs.Logs{
 
 // initGenerateYwloaderFile 初始化配置
 func initGenerateYwloaderFile(path string) bool {
-	// 没有就创建一个 youwant.json
+	// 如果没有此文件将会创建一个 youwant.json
 	if !utils.FileExists(path) {
 		file, err := os.Create(path)
 		if err != nil {
 			ilog.FileCreatedFail(err.Error())
 			return false
 		}
+		// 在函数结束后执行文件关闭
 		defer file.Close()
+		// 文件创建并打开后写入默认内容
 		file.WriteString("[]")
 		return true
 	}
 	return false
 }
 
+// InitHandler 初始化配置文件
 func InitHandler(cmd *cobra.Command, args []string) {
 	ilog.Println("初始化配置文件")
+	// 将初始化配置文件(youwant.json)，成功将打印完成，失败将打印文件已存在
 	if initGenerateYwloaderFile(youwant_json) {
 		ilog.ConfigCreatedSuccess(youwant_json)
 	} else {
