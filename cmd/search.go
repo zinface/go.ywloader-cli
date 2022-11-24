a5ae80f0c825c7846180d7ee46f7b60b73d06f79
zinface <zinface@163.com>
2022-11-24 19:52:53 +0800

    repo: 添加 'search' 子命令操作

---
+++ cmd/search.go
@@ -0,0 +1,38 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"gitee.com/zinface/go.ywloader-cli/extenstions/youwant"
+	"github.com/spf13/cobra"
+)
+
+const (
+	_desc_search = "搜索仓库项目索引"
+)
+
+// searchCmd represents the search command
+var searchCmd = &cobra.Command{
+	Use:   "search",
+	Short: _desc_search,
+	Long:  ``,
+	Run:   youwant.SearchHandler,
+}
+
+func init() {
+	rootCmd.AddCommand(searchCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// searchCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// searchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	searchCmd.Flags().BoolP("global", "g", false, "使用全局配置项")
+	searchCmd.Flags().BoolP("json", "j", false, "使用 json 格式输出结果")
+}
---
+++ extenstions/youwant/search.go
@@ -0,0 +1,58 @@
+package youwant
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+// SearchHandler 搜索可用项
+func SearchHandler(cmd *cobra.Command, args []string) {
+	var err error
+
+	// 1. 预检查传入的参数信息
+	if len(args) < 1 {
+		fmt.Fprintf(os.Stderr, "未指定参数")
+		os.Exit(1)
+	}
+
+	// 2. 获取所有条目信息
+	wants, err := loaderYouwants(cmd)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(1)
+	}
+
+	// 3. 使用 []string 存储存在关键词的条目Label
+	var arrayForLabel []string
+
+	// 以数组形式存储 Label 内容
+	// 此部分用于 arrayForJson 内容
+	for i := 0; i < len(wants); i++ {
+		// TODO：增加多关键词搜索功能
+		if strings.Contains(wants[i].Label, args[0]) {
+			arrayForLabel = append(arrayForLabel, wants[i].Label)
+		}
+	}
+
+	// 4. 尝试在此时获取 --json/-j 参数标志
+	useJson, err := cmd.Flags().GetBool("json")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(1)
+	}
+
+	// 如果 Flags 中 json 被指定将输出 json 内容
+	// 否则直接输出 Label 内容
+	if useJson {
+		arrayForJson, _ := json.Marshal(arrayForLabel)
+		fmt.Println(string(arrayForJson))
+	} else {
+		for i := 0; i < len(arrayForLabel); i++ {
+			fmt.Println(arrayForLabel[i])
+		}
+	}
+}
