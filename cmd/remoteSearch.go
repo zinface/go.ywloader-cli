84e93534e385dad42158d2adee6f0a73f1559a15
zinface <zinface@163.com>
2022-12-25 04:05:32 +0800

    repo: 添加 remote 子命令的 search 命令, 需要 --url 指定远端地址
    例如:
        ywloader remote search --all --url 'http://example.com/youwant.json'
        可通过 --all 采用全量搜索，或在尾部添加关键词进行搜索

---
+++ cmd/remoteSearch.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"gitee.com/zinface/go.ywloader-cli/extenstions/youwant/remote"
+	"github.com/spf13/cobra"
+)
+
+const (
+	_desc_remote_search = "搜索远程仓库源项目索引"
+)
+
+// remoteSearchCmd represents the remoteSearch command
+var remoteSearchCmd = &cobra.Command{
+	Use:    "search",
+	Short:  _desc_remote_search,
+	Long:   ``,
+	PreRun: remote.PreRunHandler,
+	Run:    remote.SearchHandler,
+}
+
+func init() {
+	remoteCmd.AddCommand(remoteSearchCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// remoteSearchCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// remoteSearchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	remoteSearchCmd.Flags().BoolP("all", "a", false, "输出全部配置项")
+	remoteSearchCmd.Flags().BoolP("json", "j", false, "使用 json 格式输出结果")
+	remoteSearchCmd.Flags().StringP("url", "u", "", "远程仓库源路径(http)")
+	remoteSearchCmd.MarkFlagRequired("url")
+}
